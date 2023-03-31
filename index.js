b41e6f688d13e3c2375697d537256d574e3759f0
Gabriel Manicucci <gabrielmanicucci@outlook.com>
2023-03-31 08:42:10 -0300

    add feature localStorage

--- index.js
+++ index.js
@@ -2,35 +2,69 @@ const form = document.getElementById("form")
 const input = document.getElementById("input")
 const tasks = document.querySelector(".tasks")
 
+const todos = JSON.parse(localStorage.getItem("list"))
+
+if (todos) {
+  todos.forEach(todo => addTodo(todo))
+  console.log(todos)
+}
+
 form.addEventListener("submit", event => {
   event.preventDefault()
   addTodo()
 })
 
-function addTodo() {
-  let textInput = input.value
+function addTodo(todo) {
+  let todoText = input.value
 
-  if (textInput) {
+  if (todo) {
+    todoText = todo.text
+  }
+
+  if (todoText) {
     const task = document.createElement("div")
     task.classList.add("task")
 
     const text = document.createElement("p")
-    text.innerText = textInput
+    text.classList.add("text")
+
+    if (todo && todo.completed) {
+      text.classList.toggle("completed")
+    }
+
+    text.innerText = todoText
+
+    task.addEventListener("click", () => {
+      text.classList.toggle("completed")
+      updateStorage()
+    })
+
+    task.addEventListener("contextmenu", event => {
+      event.preventDefault()
+      task.remove()
+      updateStorage()
+    })
 
     task.appendChild(text)
     tasks.appendChild(task)
 
-    task.addEventListener('click', () => {
-      text.classList.toggle('completed')
-    })
+    input.value = ""
 
-    task.addEventListener('contextmenu', event => {
-      event.preventDefault()
-      task.remove()
-    })
-
-    input.value = ''
+    updateStorage()
   }
+}
 
-  console.log(textInput)
+function updateStorage() {
+  const taskList = document.querySelectorAll(".text")
+
+  const list = []
+
+  taskList.forEach(task => {
+    list.push({
+      text: task.innerText,
+      completed: task.classList.contains("completed")
+    })
+  })
+
+  localStorage.setItem("list", JSON.stringify(list))
 }
